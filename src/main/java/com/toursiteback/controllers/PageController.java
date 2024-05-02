fca04a370d4b019361b5205ca6dd79ffbf82f4a9
Daniel160407 <abulashvilidani@hotmail.com>
2024-05-02 23:41:05 +0400

    Added PageController class and imported frontend part

---
+++ src/main/java/com/toursiteback/controllers/PageController.java
@@ -0,0 +1,27 @@
+package com.toursiteback.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+@CrossOrigin(origins = "*")
+public class PageController {
+
+    @GetMapping(value = "/tours")
+    public String getToursPage() {
+        return "index.html";
+    }
+
+    @GetMapping(value = "/adminpanel")
+    public String getAdminPanelPage() {
+        return "index.html";
+    }
+
+    @GetMapping(value = "{path:[^\\.]*}")
+    public String redirectToIndex() {
+        return "index.html";
+    }
+}
